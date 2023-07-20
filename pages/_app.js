f79358f76d1e217fea4cc81bb05a0f21ac0b5b63
Juan Carlos Velasquez Lopez <juancarlosv2711@gmail.com>
2023-07-20 02:58:19 -0600

    Setting up redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '@/styles/globals.scss'
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { Provider } from "react-redux";
+import store from "../store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+let persistor = persistStore(store);
+function MyApp({ Component, pageProps }) {
+  return (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+        <Component {...pageProps} />
+    </PersistGate>
+  </Provider>
+  );
 }
+
+export default MyApp;
