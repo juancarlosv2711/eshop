416bd23feaf059e7634f673fe86043b44ab1a109
Juan Carlos Velasquez Lopez <juancarlosv2711@gmail.com>
2023-11-01 02:15:06 -0600

    Adding login

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
-import '@/styles/globals.scss'
-import Head from 'next/head'
+import '@/styles/globals.scss';
+import Head from 'next/head';
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import store from "../store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 let persistor = persistStore(store);
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Head>
@@ -14,13 +15,15 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </SessionProvider>
     </>
- 
+
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.scss';
 import Header from "../components/header";
 import Footer from "../components/footer";
-import axios from "axios"; 
+import axios from "axios";
+import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home({country}) {
-  console.log(country);
+  const { data: session } = useSession()
   return (
     <div>
       <Header country={country}/>
